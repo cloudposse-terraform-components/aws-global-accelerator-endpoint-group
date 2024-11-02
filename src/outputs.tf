18e594cb9a7d12efb44c0eecdb32be43b13073ac
Igor Rodionov <goruha@gmail.com>
2024-11-02 19:20:00 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,4 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "id" {
+  description = "The ID of the Global Accelerator Endpoint Group."
+  value       = module.endpoint_group.id
 }
