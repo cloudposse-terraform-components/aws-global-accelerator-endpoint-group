5fc13f30609742554ec9c36fe5263957e702714f
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:23:01 +0100

    Initial commit

--- src/main.tf
+++ src/main.tf
@@ -1,8 +1,9 @@
-locals {
-  enabled = module.this.enabled
+module "endpoint_group" {
+  source  = "cloudposse/global-accelerator/aws//modules/endpoint-group"
+  version = "0.5.0"
+
+  context = module.this.context
+
+  listener_arn = module.global_accelerator.outputs.listener_ids[0]
+  config       = var.config
 }
-
-
-
-
-
